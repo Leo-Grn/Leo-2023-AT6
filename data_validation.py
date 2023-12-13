d83eb587612f21485aa7604a826383aa72b135f2
Leo <leogreengn@gmail.com>
2023-12-13 22:42:30 +0100

    Second commit: Added positive integer check

--- data_validation.py
+++ data_validation.py
@@ -1,8 +1,8 @@
 class DataValidator:
-    # Method to validate a list of strings
+    # Method to validate a list of strings for positive integers
     def validate(self, input_list):
         result = []  # Initialize an empty list for valid numbers
         for item in input_list:
-            if item.isdigit():  # Check if the item is a digit
-                result.append(int(item))  # Add the integer to the result list
-        return result  # Return the list of integers
+            if item.isdigit() and int(item) > 0:  # Check if the item is a positive digit
+                result.append(int(item))  # Add the positive integer to the result list
+        return result  # Return the list of positive integers
